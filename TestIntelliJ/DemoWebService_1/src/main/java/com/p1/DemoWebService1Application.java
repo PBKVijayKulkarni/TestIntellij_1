2ee502929e805698b618586175d50d5a8930bbc9
Vijay K <vijaykulkarni2207@gmail.com>
2023-07-30 22:00:13 +0530

    1st commit. Empty Spring boot project

---
+++ TestIntelliJ/DemoWebService_1/src/main/java/com/p1/DemoWebService1Application.java
@@ -0,0 +1,13 @@
+package com.p1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebService1Application.class, args);
+	}
+
+}
